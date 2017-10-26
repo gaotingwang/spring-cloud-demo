51d814032f6765d9a28d4913e002a952343cd273
gaotingwang <gaotingwang@jd.com>
2017-10-26 09:58:54 +0800

    重命名

--- eureka-producer/src/main/java/com/gtw/client/EurekaProducerApplication.java
+++ eureka-producer/src/main/java/com/gtw/client/EurekaProducerApplication.java
@@ -6,8 +6,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient // 该注解能激活Eureka中的DiscoveryClient实现，这样才能实现Controller中对服务信息的输出。
-public class ClientApplication {
+public class EurekaProducerApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ClientApplication.class, args);
+        SpringApplication.run(EurekaProducerApplication.class, args);
     }
 }

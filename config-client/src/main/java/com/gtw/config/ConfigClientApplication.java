8e2e62443b97131a508833b6baf6f1350f728c42
gaotingwang <gaotingwang@jd.com>
2017-11-03 22:58:14 +0800

    配置文件数据获取刷新支持

--- config-client/src/main/java/com/gtw/config/ConfigClientApplication.java
+++ config-client/src/main/java/com/gtw/config/ConfigClientApplication.java
@@ -3,6 +3,7 @@ package com.gtw.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,7 @@ public class ConfigClientApplication {
         SpringApplication.run(ConfigClientApplication.class, args);
     }
 
+    @RefreshScope
     @RestController
     class HelloController {
         @Value("${demo.active}")
